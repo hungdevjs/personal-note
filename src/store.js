0da9b90e8d5e0db81403f8b5aa272ef7cdd508c0
hungdevjs <hungdev.js@gmail.com>
2020-11-24 22:44:51 +0700

    remove cmt

--- src/store.js
+++ src/store.js
@@ -1,14 +1,7 @@
 
 import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
+import logger from "redux-logger"
 
 import rootReducer from "./rootReducer"
-
-let middleware = [thunk]
-if (process.env.NODE_ENV !== "production") {
-    const logger = require("redux-logger")
-    middleware = [...middleware, logger]
-}
-
-// export default createStore(rootReducer, applyMiddleware(thunk, logger))
-export default createStore(rootReducer, applyMiddleware(...middleware))
+export default createStore(rootReducer, applyMiddleware(thunk, logger))
