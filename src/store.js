131e85ba9188ba1f7d654dd686524f80f52910fd
hungdevjs <hungdev.js@gmail.com>
2020-11-25 19:06:40 +0700

    fix middleware store

--- src/store.js
+++ src/store.js
@@ -3,4 +3,11 @@ import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 
 import rootReducer from "./rootReducer"
-export default createStore(rootReducer, applyMiddleware(thunk))
+
+let middleware = [thunk]
+if (process.env.NODE_ENV !== "production") {
+    const logger = require("redux-logger")
+    middleware = [...middleware, logger]
+}
+
+export default createStore(rootReducer, applyMiddleware(...middleware))
